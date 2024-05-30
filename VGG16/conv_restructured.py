7ac732c123d09d1afb1e0e4ca484b9ebd24f3d78
MuthuAjay <muthuajay97@gmail.com>
2024-05-30 18:40:49 +0530

    added conv2d class with back propagation

--- VGG16/conv_restructured.py
+++ VGG16/conv_restructured.py
@@ -48,7 +48,7 @@ class Softmax:
     def __call__(self,
                  X: torch.Tensor,
                  dim: int):
-        X = X - torch.max(X ,dim=1, keepdims=True).values
+        X = X - torch.max(X, dim=1, keepdims=True).values
         sof = torch.exp(X) / torch.sum(torch.exp(X), dim=dim, keepdims=True)
 
     def parameters(self):
@@ -58,8 +58,8 @@ class Softmax:
 class OptimizerSG:
 
     def __init__(self,
-                params: Optional[List],
-                lr : float = 0.1):
+                 params: Optional[List],
+                 lr: float = 0.1):
         self.params = params
         self.lr = lr
 
@@ -157,3 +157,167 @@ class MaxPool2d:
 
     def parameters(Self):
         return []
+
+
+# Custom Conv2d Layer
+class Conv2d:
+    def __init__(self,
+                 in_channels: int,
+                 out_channels: int,
+                 kernel_size: int,
+                 stride: int = 1,
+                 padding: int = 0,
+                 dilation: int = 1,
+                 groups: int = 1):
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.groups = groups
+        self._check_parameters()
+        self._n_tuple()
+        self.weights, self.bias = self.initialise_weights()
+
+    def _n_tuple(self):
+        self.kernel_size = (self.kernel_size, self.kernel_size)
+        self.stride = (self.stride, self.stride)
+        self.padding = (self.padding, self.padding)
+        self.dilation = (self.dilation, self.dilation)
+
+    def initialise_weights(self):
+        return (torch.randn(self.out_channels, self.in_channels // self.groups, *self.kernel_size, requires_grad=True),
+                torch.zeros(self.out_channels, requires_grad=True))
+
+    def add_padding(self, x: torch.Tensor, padding: int):
+        padding = tuple(repeat(padding, 4))
+        batch_size, in_channels, original_height, original_width = x.size()
+        padded_height = original_height + padding[0] + padding[1]
+        padded_width = original_width + padding[2] + padding[3]
+
+        if (self.padded_height and self.padded_width) is None:
+            self.padded_height, self.padded_width = padded_height, padded_width
+
+        padded_x = torch.zeros((batch_size, in_channels, padded_height, padded_width), dtype=x.dtype)
+        padded_x[:, :, padding[0]:padding[0] + original_height, padding[2]:padding[2] + original_width] = x
+        return padded_x
+
+    def _check_parameters(self):
+        if self.groups <= 0:
+            raise ValueError('groups must be a positive integer')
+        if self.in_channels % self.groups != 0:
+            raise ValueError('in_channels should be divisible by groups')
+        if self.out_channels % self.groups != 0:
+            raise ValueError('out_channels should be divisible by groups')
+
+    def __call__(self, x: torch.Tensor):
+        self.X = x
+        self.ih, self.iw = self.X.shape[-2], self.X.shape[-1]
+        batch_size, in_channels, in_height, in_width = x.size()
+        out_height = (in_height + 2 * self.padding[0] - self.dilation[0] * (self.kernel_size[0] - 1) - 1) // \
+                     self.stride[0] + 1
+        out_width = (in_width + 2 * self.padding[1] - self.dilation[1] * (self.kernel_size[1] - 1) - 1) // self.stride[
+            1] + 1
+
+        if self.padding[0] > 0 or self.padding[1] > 0:
+            x = self.add_padding(x, self.padding[0])
+
+        out = torch.zeros(batch_size, self.out_channels, out_height, out_width)
+
+        for h in range(out_height):
+            for w in range(out_width):
+                h_start = h * self.stride[0]
+                h_end = h_start + self.kernel_size[0]
+                w_start = w * self.stride[1]
+                w_end = w_start + self.kernel_size[1]
+                receptive_field = x[:, :, h_start:h_end, w_start:w_end]
+
+                out[:, :, h, w] = torch.sum(
+                    receptive_field.unsqueeze(1) * self.weights.view(1, self.out_channels,
+                                                                     self.in_channels // self.groups,
+                                                                     *self.kernel_size),
+                    dim=(2, 3, 4)
+                ) + self.bias.view(1, self.out_channels)
+
+        return out
+
+    def prepare_subMatrix(self, X, Kh, Kw, s):
+        B, C, ih, iw = X.shape
+        sh, sw = s
+
+        Oh = (ih - Kh) // sh + 1
+        Ow = (iw - Kw) // sw + 1
+
+        strides = (C * ih * iw, iw * ih, iw * sh, sw, iw, 1)
+        subM = torch.as_strided(X,
+                                shape=(B, C, Oh, Ow, Kh, Kw),
+                                strides=strides
+                                )
+        return subM
+
+    def padding_backward(self,
+                         dXp: torch.Tensor):
+
+        B, C, ih, iw = self.X.shape
+        dX = dXp[:, :, self.padded_height:ih, self.padded_width:iw]
+        return dX
+
+    def convolve(self, X: torch.Tensor,
+                 K: torch.Tensor,
+                 s: Tuple = (1, 1),
+                 mode: str = 'back'):
+
+        F, Kc, Kh, Kw = K.shape
+        subM = self.prepare_subMatrix(X, Kh, Kw, s)
+
+        if mode == 'front':
+            return torch.einsum('fckl,mcijkl->mfij', K, subM)
+        elif mode == 'back':
+            return torch.einsum('fdkl,mcijkl->mdij', K, subM)
+        elif mode == 'param':
+            return torch.einsum('mfkl,mcijkl->fcij', K, subM)
+
+    def dz_D_dx(self,
+                dZ: torch.Tensor,
+                ih: int,
+                iw: int):
+        _, _, Hd, Wd = dZ.shape
+        ph = ih - Hd + self.kernel_size[0] - 1
+        pw = iw - Wd + self.kernel_size[0] - 1
+
+        dZ_Dp = self.add_padding(dZ, ph)
+
+        # Rotate the Kernel by 180 degrees
+        k_rotated = self.weights[:, :, ::-1, ::-1]
+
+        # convolve w.r.t k_rotated
+        dXp = self.convolve(dZ_Dp, k_rotated, mode='back')
+
+        dX = self.padding_backward(dXp)
+        return dX
+
+    def backward(self,
+                 dZ: torch.Tensor):
+        Xp: torch.Tensor = self.add_padding(self.X, self.padding)
+        B, C, ih, iw = Xp.shape
+
+        # dZ -> dZ_D_dX
+        dX = self.dz_D_dx(dZ, ih, iw)
+
+        # gradient dK
+        _, _, Hd, Wd = dZ.shape
+        ph = self.ih - Hd - self.kernel_size[0] + 1
+        pw = self.iw - Wd - self.kernel_size[0] + 1
+
+        dZ_Dp = self.add_padding(dZ, padding=ph)
+
+        self.dweights = self.convolve(Xp, dZ_Dp, mode='param')
+
+        # gradient db
+        self.dbias = dZ.sum(0)
+
+        return dX
+
+    def parameters(self):
+        return [self.weights] + ([] if self.bias is None else [self.bias])
