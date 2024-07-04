c17ac5d137ca47aac8f86ce9e0bc5f469eca414f
MuthuAjay <muthuajay97@gmail.com>
2024-07-05 00:21:15 +0530

    CNN version 2 - Softmax

--- CNN_Version2/activation.py
+++ CNN_Version2/activation.py
@@ -20,13 +20,19 @@ class Relu:
 class Sigmoid:
 
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        sel
         return 1 / (1 + torch.exp(-x))
 
-    def backward(self, dz: torch.Tensor)-> torch.Tensor:
-        return self(dz) * (1-self(dz))
+    def backward(self, dz: torch.Tensor) -> torch.Tensor:
+        return self(dz) * (1 - self(dz))
 
 
 class Softmax:
 
-    def __call__(self, x:torch.Tensor)->torch.Tensor:
-        pass
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        x = x - x.max(dim=1, keepdim=True).values
+        self.out = torch.exp(x) / torch.sum(torch.exp(x), dim=1, keepdim=True)
+        return self.out
+
+    def parameters(self):
+        return []
