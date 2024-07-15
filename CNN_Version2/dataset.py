1457dbe825fa94d9be93261e7f2819e07242caa1
MuthuAjay <muthuajay97@gmail.com>
2024-07-15 16:38:32 +0530

    added flatten.py

---
+++ CNN_Version2/dataset.py
@@ -0,0 +1,23 @@
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Compose
+from torch.utils.data import DataLoader, SubsetRandomSampler
+
+
+def dataset():
+    training_data = datasets.FashionMNIST(
+        root='data',
+        train=True,
+        download=True,
+        transform=Compose([ToTensor()])
+    )
+
+    test_data = datasets.FashionMNIST(
+        root='data',
+        train=False,
+        download=True,
+        transform=Compose([ToTensor()])
+    )
+
+    traindataloader = DataLoader(training_data, batch_size=64, shuffle=False, sampler=SubsetRandomSampler(range(100)))
+    testdataloader = DataLoader(test_data, batch_size=64, shuffle=False, sampler=SubsetRandomSampler(range(20)))
+    return traindataloader, testdataloader
